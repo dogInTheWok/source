fe362f1d4f1609f3fe8f2263ff25e4f44947c1a5
MattW <ab.ins.licht@gmx.de>
2013-01-26 14:38:25 +0100

    Backup. Drawing works.

--- game.js
+++ game.js
@@ -8,10 +8,11 @@ var ctx=c.getContext("2d");
 var XOFF=7;					// canvas offsets, irgendwie elegant kalibrieren!
 var YOFF=105;
 var TOLERANCE=5;
+var MIN_DISTANCE_TO_START = 40;
 var XCENTER=360;
 var YCENTER=250;
 var RADIUS=240;
-var RADIUS_MARGIN = 8000;
+var RADIUS_MARGIN = 8000;http://www.notebooksbilliger.de/notebooks/lenovo+notebooks/lenovo+business/thinkpad+l+serie
 
 // global coordinates
 var lineValid=true;
@@ -25,6 +26,7 @@ var myLines = new Array();
 
 ///Length of each actual line
 var lineLength = new Array();
+lineLength[0]=0;
 
 /* Storage for the intersection points between the lines
  * 2*i	   = x coordinate
@@ -48,7 +50,7 @@ var currentIdent = 0;
 var identLine = 0;
 
 // debug auxiliary variables/functions
-var hitLineAt=0;
+;
 
 // Handle keyboard controls
 var keysDown = {};
@@ -65,8 +67,7 @@ addEventListener("mouseup", function (e) {
 
 function drawCircle()
 {
-	var c=document.getElementById("cw");
-	var ctx=c.getContext("2d");
+	ctx.fillStyle = '#000000';
 	ctx.beginPath();
 	ctx.arc(XCENTER,YCENTER,RADIUS,0,2*Math.PI);
 	ctx.stroke();
@@ -86,10 +87,28 @@ function drawLine(fromx, fromy, tox, toy, color)
 
 function eraseInvalid()
 {
-	for (i=0;i<currentLine.length-1;i++)
+	var x;
+	var y;
+	var nextX;
+	var nextY;
+	var start=0;
+	
+	ctx.clearRect(0,0,1000,1000);
+	ctx.beginPath();
+	
+	drawCircle();
+	
+	for (t=0;t<lineLength.length;t++)
 	{
-		drawLine(currentLine[3*i], currentLine[3*i+1], \
-			currentLine[3*(i+1)], currentLine[3*(i+1)+1,'#FFFFFF']);
+		for (i=start;i<start+lineLength[t]-1;i++)
+		{
+			nextX=myLines[3*(i+1)];
+			nextY=myLines[3*(i+1) + 1];
+			x=myLines[3*i];
+			y=myLines[3*i+1];
+			drawLine(x,y,nextX,nextY,'#000000');
+		}
+		start=start+lineLength[t];
 	}
 }
 
@@ -108,14 +127,13 @@ function finalizeLine()
 	/// - line itself
 	for(ind = 0; ind < currentIdent; ++ind)
 	{
-		myLines[3 * ind] = currentLine[3 * ind]; 
-		myLines[3 * ind + 1] = currentLine[3 * ind + 1]; 
-		myLines[3 * ind + 2] = identLine + ind; 
+		myLines[(identLine + ind) * 3] = currentLine[3 * ind]; 
+		myLines[(identLine + ind) * 3 + 1] = currentLine[3 * ind + 1]; 
+		myLines[(identLine + ind) * 3 + 2] = identLine + ind; 
 	}
-	lineLength[counterLines] = currentIdent + 1;
+	lineLength[counterLines] = currentIdent;
 	counterLines++;
-	identLine += currentIdent;
-	currentIdent = 0;
+	identLine = identLine + currentIdent;
 	
 	/// - startInd, endInd
 	intersectioner[2 * numInter] = startInter[0];
@@ -123,152 +141,174 @@ function finalizeLine()
 	numInter++;
 	intersectioner[2 * numInter] = endInter[0];
 	intersectioner[2 * numInter + 1] = endInter[1];
-	
+	numInter++;
 }
 
 function resetStorage()
-{
+{	
+	currentLine = new Array();
 	currentIdent = 0;
+	//document.getElementById("div").innerHTML="resetStorage " + currentLine[currentLine.length-4]
 	lineValid=true;
 }
 
-function draw(e){
+function endLine()
+{
+	delete keysDown[0];
+}
+
+function checkMinDist(x, y, originX, originY, Dist)
+{
+	if((x-originX) * (x-originX) + (y -originY) * (y - originY) > Dist * Dist)
+	{
+		var checkMinDist = true;
+	}
+	else{
+		var checkMinDist = false;
+	}
+	
+	return checkMinDist;
+}
+
+function draw(e)
+{
+	if (!(0 in keysDown))
+	{
+		eraseInvalid();
+		resetStorage();
+		endLine();
+		//document.getElementById("div").innerHTML="lineValid " + lineValid
+		return;
+	}
+	
+	//document.getElementById("div").innerHTML="Joined draw(e)"
 	var x=e.clientX;
 	var y=e.clientY;
 	var cX=x-XOFF;
 	var cY=y-YOFF;
+	var prevX;
+	var prevY;
 	var rSqCur=(cX-XCENTER)*(cX-XCENTER)+(cY-YCENTER)*(cY-YCENTER);
 	
 	if(!lineValid)
 	{
+		//document.getElementById("div").innerHTML="lineValid == false"
 		///Invalid line has been drawn --> reset temp storage
 		eraseInvalid();
 		resetStorage();
-		
-	}
-	
-	
-	// check if line starts on circle
-	if ((rSqCur<RADIUS_MARGIN+RADIUS*RADIUS) && (rSqCur>RADIUS*RADIUS-RADIUS_MARGIN))
-	{
-	//	lineValid=true;
-		startInter[0] = cX;
-		startInter[1] = cY;
-		
-		///Store temporary coordinates for line here too
-		writeToTempStorage(cX, cY);
-		//drawLine();
 		return;
 	}
-	else
+	
+	if (currentIdent<=lineLength[counterLines])
 	{
-		if (i<=lineLength[counterLines])
+		// check if line starts on circle
+		if ((rSqCur<RADIUS_MARGIN+RADIUS*RADIUS) && (rSqCur>RADIUS*RADIUS-RADIUS_MARGIN))
+		{
+			//document.getElementById("div").innerHTML="Start on cirlce arc"
+			startInter[0] = 4 * numInter; //cX;
+			startInter[1] = 4 * numInter + 1; //cY;
+			
+			///Store temporary coordinates for line here too
+			writeToTempStorage(cX, cY);
+			//document.getElementById("div").innerHTML="RETURN 1"
+			return;
+		}
+		else
 		{
 			lineValid=false;
+			//document.getElementById("div").innerHTML="RETURN 2"
 			return;
 		}
 	}
-	
-	for (l=0;l<myLines.length/3-10;l++)
+	else
 	{
+		if (rSqCur>RADIUS*RADIUS)
+		{
+			//if( checkMinDist(cX,cY,currentLine[0],currentLine[1],MIN_DISTANCE_TO_START))
+			{
+				endInter[0] = cX;
+				endInter[1] = cY;
+				
+				writeToTempStorage(cX, cY);
+				
+				finalizeLine();
+				endLine();
+				resetStorage();
+			}
+			
+		}
+	}
+	for (l = 0; l < Math.max(myLines.length, currentLine.length) / 3 - 10; l++)
+	{
+		//document.getElementById("div").innerHTML=""+myLines[150]+" "+myLines[151];
+		document.getElementById("div1").innerHTML="" + identLine;
+		document.getElementById("div2").innerHTML="" + currentIdent;
 		// check if line starts on another line
 		if (currentIdent<=lineLength[counterLines])
 		{
+			document.getElementById("div").innerHTML="start on another line"
 			if (cX<myLines[3*l]+TOLERANCE && cX>myLines[3*l]-TOLERANCE && cY<myLines[3*l+1]+TOLERANCE && cY>myLines[3*l+1]-TOLERANCE)
 			{
-				startInter[0] = cX;
-				startInter[1] = cY;
+				startInter[0] = 4 * numInter + 2; //cX;
+				startInter[1] = 4 * numInter + 3; //cY;
 				
+				//document.getElementById("div").innerHTML="Start on another line"
+								
 				///Store temporary coordinates for line here
 				writeToTempStorage(cX, cY);
-				drawLine(cX,cY,currentLine[3*(currentIdent - 1)], \ 
-				currentLine[3*(currentIdent - 1) + 1],'#000000');
+				
 			}
 			else
 			{
 				lineValid=false;
 			}
+			//document.getElementById("div").innerHTML="RETURN 3"
 			return;
 		}
-		
-		if(currentIdent > lineLength[counterLines] + 8 * TOLERANCE)
+		if (l<currentLine.length/3-10 && currentIdent>2 && (cX<currentLine[3*l]+TOLERANCE && cX>currentLine[3*l]-TOLERANCE && cY<currentLine[3*l+1]+TOLERANCE && cY>currentLine[3*l+1]-TOLERANCE))
+		{
+				endInter[0] = cX;
+				endInter[1] = cY;
+				
+				 ///Store temporary coordinates for line here
+			  	writeToTempStorage(cX, cY);
+
+				 ///Check if valid and finalize
+				eraseInvalid();
+				resetStorage();
+				endLine();
+				document.getElementById("div").innerHTML="ich schie� mir selbst in den Fu�"
+				return;
+		}
+		if(checkMinDist(cX,cY,currentLine[0],currentLine[1],MIN_DISTANCE_TO_START))
 		{
 			// check if line crosses another line
 			if (cX<myLines[3*l]+TOLERANCE && cX>myLines[3*l]-TOLERANCE && cY<myLines[3*l+1]+TOLERANCE && cY>myLines[3*l+1]-TOLERANCE)
 			{
-				//hitLine=true;
-				hitLineAt=l;
+				
 				endInter[0] = cX;
 				endInter[1] = cY;
 				 ///Store temporary coordinates for line here
 			  	writeToTempStorage(cX, cY);
-				drawLine(cX,cY,currentLine[3*(currentIdent - 1)], \ 
-				currentLine[3*(currentIdent - 1) + 1],'#000000');
+
 				 ///Check if valid and finalize
 				finalizeLine();
-				
-				break;
+				resetStorage();
+				endLine();
+				return;
 			}
 		}
 	}
+	
 	///Store temporary line coordinates here
 	writeToTempStorage(cX, cY);
-	drawLine(cX,cY,currentLine[3*(currentIdent - 1)], \ 
-				currentLine[3*(currentIdent - 1) + 1],'#000000');
+	prevX=currentLine[3*(currentIdent - 2)];
+	prevY=currentLine[3*(currentIdent - 2) + 1];
+	//document.getElementById("div").innerHTML="" + prevX + " " + prevY;
+	if (currentIdent>1)
+	{
+		drawLine(cX,cY,prevX,prevY,'#000000');
+	}
 	
 }
-	///////////////////
-	// draw line if inside circle and no condition to cancel is triggered
-/*	if ((0 in keysDown) && (rSqCur<=RADIUS*RADIUS) && lineValid)
-	{
-		ctx.fillStyle = '#000000';
-		ctx.moveTo(cX,cY)
-		ctx.lineTo(global_X-XOFF,global_Y-YOFF);
-		ctx.stroke();
-		global_X=e.clientX;
-		global_Y=e.clientY;
-		myLines[3*i]=cX;
-		myLines[3*i+1]=cY;
-		myLines[3*i+2]=k;
-		i++;
-		drawn=true;
-	}
-	else
-	{
-		global_X=e.clientX;
-		global_Y=e.clientY;
-		lineLength[k]=i;
-		if (lineValid && drawn)
-		{
-			document.getElementById("div").innerHTML="k: "+String(k)+"; LL[k]: "+lineLength[k]+", "+String(lineLength[k-1])+", "+myLines.length+"<br>Hitline: "+String(hitLine)+" @ "+String(hitLineAt)
-			endInter[0] = cX;
-			endInter[1] = cY;
-			k++;
-			
-		}
-		lineValid=false;
-		drawn=false;
-		delete keysDown[e.keyCode];
-		intersectioner[numInter*2]=endInter[0];
-	//	if (i>0)
-	//	{
-	//		for(j=0;j<line.length;j++)
-	//		{
-	//			if (j%5==0)
-	//			{
-	//				document.getElementById("div").innerHTML=document.getElementById("div").innerHTML+line[j]+"<br>"
-	//				document.getElementById("div").innerHTML=i;	
-	//			}
-	//		}
-	//		myLines[k]=line.length;
-	//		myLines[k+1]=line;
-	//		i=0;
-	//		k=k+line.length;
-	//		document.getElementById("div").innerHTML=""
-	//	}
-		
-	}
-
-}
-*/
 
